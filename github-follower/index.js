69b83e016bade2622642c5f2241de73aa26dc97e
gunturkh <guntur.kh@gmail.com>
2018-08-01 10:55:08 +0700

    Refractoring code

--- github-follower/index.js
+++ github-follower/index.js
@@ -4,30 +4,27 @@ const url = "https://api.github.com/users/gunturkh/followers"
 $(function () {
 
     fetch(url)
-        .then(response => {
-            return response.json();
-        })
+        .then(response => {return response.json();})
         .then(data => {
-            let results = data
-
             $("#output").html("")
-            results.map((result, index) => {
-                $("#output").append(`
-                
-                <div class="col-md-auto">
-                    <div class="card">
-                        <img class="card-img-top" src="${results[index].avatar_url}" alt="Card image cap">
-                        <div class="card-body">
-                            <h3 class="card-title text-center">${results[index].login}</h3>
-                            <h3 class="card-text text-center">${results[index].id}</h3>
-                            <a href="${results[index].url}" class="btn btn-primary btn-block">Go to profile</a>
-                        </div>
-                    </div>
-                </div>
-                
-                `)
-                console.log(results[index])
+            data.forEach(item => {
+                $("#output").append(template(item))
+                console.log(item)
             })
         });
+        const template = (item => {
+            return `
+            <div class="col-md-auto">
+                <div class="card">
+                    <img class="card-img-top" src="${item.avatar_url}" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title text-center">${item.login}</h3>
+                        <h3 class="card-text text-center">${item.id}</h3>
+                        <a href="${item.url}" class="btn btn-primary btn-block">Go to profile</a>
+                    </div>
+                </div>
+            </div>
+            `
+        })
 
 })
